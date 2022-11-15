930fa956ec7904dafd145de2786a8d0feb3b0fda
adityajoshigit <adityasjoshi96@gmail.com>
2022-11-15 00:05:42 -0500

    added VideoList cmp

--- src/App.vue
+++ src/App.vue
@@ -3,37 +3,44 @@
         <SearchBar 
             @searchQueryChange="handleSearchQueryChange"
         />
+
+        <div id="videoListContainer" class="video-list-div">
+            <VideoList />
+        </div>
     </div>
 </template>
 
 <script>
-import axios from 'axios';
+// import axios from 'axios';
 import SearchBar from './components/SearchBar.vue';
-const API_KEY = 'AIzaSyAUCwdsA3NrhbcA5g2LUf5JT3nZ5Pk1ZlE';
+import VideoList from './components/VideoList.vue';
+// const API_KEY = 'AIzaSyAUCwdsA3NrhbcA5g2LUf5JT3nZ5Pk1ZlE';
 
 export default {
     name: 'App',
     components: {
-        SearchBar: SearchBar
+        SearchBar: SearchBar,
+        VideoList: VideoList
     },
     methods: {
         handleSearchQueryChange(event) {
             console.log('search term = ', event.searchQuery);
-            const searchTerm = event.searchQuery;
-            axios.get(
-                'https://www.googleapis.com/youtube/v3/search',
-                {
-                    params: {
-                        key: API_KEY,
-                        type: 'video',
-                        part: 'snippet',
-                        q: searchTerm
-                    }
-                }
-            )
-            .then(resp => {
-                console.log(resp);
-            })
+            
+            // const searchTerm = event.searchQuery;
+            // axios.get(
+            //     'https://www.googleapis.com/youtube/v3/search',
+            //     {
+            //         params: {
+            //             key: API_KEY,
+            //             type: 'video',
+            //             part: 'snippet',
+            //             q: searchTerm
+            //         }
+            //     }
+            // )
+            // .then(resp => {
+            //     console.log(resp);
+            // });
         }
     }
 }
---
+++ src/components/VideoList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="videoListCmp">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'VideoList'
+}
+</script>
+
+<style>
+
+</style>
