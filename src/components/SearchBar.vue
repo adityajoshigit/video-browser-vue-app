458a1693630954807c69762d560b895186326658
adityajoshigit <adityasjoshi96@gmail.com>
2022-11-14 23:15:00 -0500

    started with the basic structure of a vue application and its components

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="searchBarCmp">
+        <input 
+            type="text" 
+            name="search-query-tag" 
+            id="searchQueryTextBox" 
+            placeholder="Some search query goes here..."
+            @input="onSearchQueryInput" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchBar',
+    methods: {
+        onSearchQueryInput(event) {
+            console.log(event.target.value);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
